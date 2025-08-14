f2abc50142f9bab3e72b499300f46c9fd0bd7a70
fred <fredteumer@gmail.com>
2025-08-14 11:02:51 -0400

    updating billing estimate to factor for the 150,000 free relays

--- app/routes/account.$accountId.billing/route.tsx
+++ app/routes/account.$accountId.billing/route.tsx
@@ -15,6 +15,8 @@ import { seo_title_append } from "~/utils/seo"
 import { requireUser } from "~/utils/user.server"
 import { UsageRecordSummary, InvoiceUsageData } from "~/types/stripe-custom"
 
+//TODO: Figure out how to have anyone who has _ever_ had PLAN_UNLIMITED (has at least 1 invoice)
+// always maintain access to the billing page. 
 export const meta: MetaFunction = () => {
   return [
     {
