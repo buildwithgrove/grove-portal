2ac6761b38f3268d4affc51f8669782309710389
fred <fredteumer@gmail.com>
2025-09-11 14:11:04 -0400

    updating limits based on pricing updates

--- app/utils/planUtils.ts
+++ app/utils/planUtils.ts
@@ -2,7 +2,7 @@ import { PayPlanType } from "~/models/portal/sdk"
 
 // TODO: Implement populating FREE_TIER_MONTHLY_RELAY_LIMIT from Portal DB
 // Currently hardcoded but should be configurable via database
-export const FREE_TIER_MONTHLY_RELAY_LIMIT = 150000
+export const FREE_TIER_MONTHLY_RELAY_LIMIT = 1000000
 
 export function isEnterprisePlan(planType: PayPlanType) {
   return planType === PayPlanType.Enterprise
